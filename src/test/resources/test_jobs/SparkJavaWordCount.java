69cc54737892968051a0906a4d55fd56ea055c50
raulgracia <raulgraciatinedo@gmail.com>
2017-05-08 15:22:35 +0200

    Start working on the use of javaslang data structures.

--- src/test/resources/test_jobs/SparkJavaWordCount.java
+++ src/test/resources/test_jobs/SparkJavaWordCount.java
@@ -1,16 +1,26 @@
 package test.resources.test_jobs;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+import java.util.Arrays;
+
 public class SparkJavaWordCount {
 	
 	public static void main(String[] args) {
 		
-		/*JavaRDD<String> textFile = sc.textFile("hdfs://...");
+		SparkConf conf = new SparkConf().setAppName("SparkJavaWordCount");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		JavaRDD<String> textFile = sc.textFile("swift2d://data1.lvm/hamlet.txt");
 		JavaPairRDD<String, Integer> counts = textFile
 		    .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
 		    .mapToPair(word -> new Tuple2<>(word, 1))
 		    .reduceByKey((a, b) -> a + b);
-		counts.saveAsTextFile("hdfs://...");*/
-		
+		counts.saveAsTextFile("swift2d://data1.lvm/hamlet_result.txt");		
 	}
-
 }
