ba553473533c1a18fc626cb77fc966380aa2b873
raulgracia <raulgraciatinedo@gmail.com>
2017-05-11 13:15:44 +0200

    Missing file.

--- src/main/java/rules/migration/javastreams/Filter.java
+++ src/main/java/rules/migration/javastreams/Filter.java
@@ -1,12 +1,3 @@
 package main.java.rules.migration.javastreams;
 
-import main.java.graph.GraphNode;
-import main.java.rules.LambdaRule;
-
-public class Filter implements LambdaRule {
-	
-	@Override
-	public void applyRule(GraphNode graphNode) {
-		graphNode.setToPushdown(graphNode.getLambdaSignature());
-	}
-}
+public class Filter extends SimpleMigrationRule {}
---
+++ src/main/java/rules/modification/javastreams/Reduce.java
@@ -0,0 +1,12 @@
+package main.java.rules.modification.javastreams;
+
+import main.java.graph.GraphNode;
+
+public class Reduce extends ActionModificationRuleJavastreams {
+
+	@Override
+	public void applyRule(GraphNode graphNode) {
+		//Add map to adapt the proper type
+		super.applyRule(graphNode);
+	}
+}
