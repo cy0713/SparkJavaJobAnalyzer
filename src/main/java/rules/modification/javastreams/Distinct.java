7b0fc0bd59df48fe3388a86867e6b1e158cc2492
raulgracia <raulgraciatinedo@gmail.com>
2017-05-17 16:34:23 +0200

    Missing file.

---
+++ src/main/java/rules/modification/javastreams/Distinct.java
@@ -0,0 +1,10 @@
+package main.java.rules.modification.javastreams;
+
+import main.java.graph.GraphNode;
+import main.java.rules.LambdaRule;
+
+public class Distinct implements LambdaRule {
+
+	@Override
+	public void applyRule(GraphNode graphNode) {}
+}
